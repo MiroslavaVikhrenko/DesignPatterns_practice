c3cd925723612b9f80506f2672141f05b8e587d4
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-07-01 12:32:50 -0600

    Practice Singleton design pattern

---
+++ DesignPattern_Singleton/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_Singleton
+{
+    public class Logger
+    {
+        private static Logger _logger; // Changed to static to resolve CS0120  
+        private readonly string _logFilePath;
+
+        private Logger()
+        {
+            _logFilePath = "log.txt";
+        }
+
+        public static Logger GetInstance()
+        {
+            if (_logger == null)
+            {
+                _logger = new Logger();
+            }
+            return _logger;
+        }
+        public void Log(string message)
+        {
+            string messageWithDateTime = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {message}\n";
+            File.AppendAllText(_logFilePath, messageWithDateTime);
+        }
+    }
+}
---
+++ DesignPattern_Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace DesignPattern_Singleton
+{
+    /*
+     Вам необходимо реализовать простой логгер, который записывает действия пользователя в текстовый файл. 
+    При этом важно, чтобы во всём приложении использовался единственный экземпляр логгера — 
+    для этого примените паттерн Singleton.
+
+Требования:
+
+1) Класс Logger должен реализовывать паттерн Singleton: приватный конструктор, 
+    статическое поле для хранения единственного экземпляра, публичный метод или свойство для доступа к экземпляру. 
+2) Метод Log(string message) записывает переданное сообщение в файл log.txt (в директории рядом с приложением), 
+    добавляя текущее время.
+3) Создайте тестовый класс Program, который вызывает Logger.Instance.Log(...) из разных мест 
+    (например, из разных методов).
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.GetInstance().Log("Application started.");
+            DoTask1();
+            DoTask2();
+            Logger.GetInstance().Log("Application completed.");
+        }
+
+        static void DoTask1()
+        {
+            Logger.GetInstance().Log("Task 1 done");
+        }
+
+        static void DoTask2()
+        {
+            Logger.GetInstance().Log("Task 2 done");
+        }
+    }
+}
